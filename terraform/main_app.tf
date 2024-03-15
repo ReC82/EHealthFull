8a329cce9134d197480e5e23e75756bdcd9d405e
ReC82 <lloyd.malfliet@gmail.com>
2024-03-15 15:24:32 +0100

    var edit

--- terraform/main_app.tf
+++ terraform/main_app.tf
@@ -12,11 +12,11 @@ module "vnet" {
 }*/
 
 module "db" {
-  source              = "github.com/ReC82/Terraform/tree/main/azure/lab04/module_vnet"
-  rg_name             = azurerm_resource_group.rg.name
-  address_space_lab04 = ["10.0.0.0/16"]
-  subnet_prefix_lab04 = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names_lab04  = ["sub1", "sub2", "sub3"]
+  source          = "./module_vnet"
+  rg_name         = var.ehealth_rg_name
+  vnet_ip_range   = var.ehealth_vnet_ip_range
+  subnet_prefixes = var.ehealth_subnets
+  subnet_names    = var.ehealth_subnets_names
 
 }
 /*
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,3 +14,18 @@ variable "resource_group_name" {
 variable "ehealth_rg_name" {
   description = "Resource group name"
 }
+
+variable "ehealth_vnet_ip_range" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable "ehealth_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "ehealth_subnets_names" {
+  type    = list(string)
+  default = ["sub-app", "sub-api", "sub-db"]
+}

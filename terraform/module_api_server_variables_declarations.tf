795eaaa007775d7da409715b50451ea5968c0f15
ReC82 <lloyd.malfliet@gmail.com>
2024-03-18 17:44:54 +0100

    format

--- terraform/module_api_server_variables_declarations.tf
+++ terraform/module_api_server_variables_declarations.tf
@@ -1,76 +1,76 @@
 # GLOBALS
 
 variable "app_ressource_group_name" {
-    description = "App Resource Group Name"
+  description = "App Resource Group Name"
 }
 
 variable "app_location" {
-    description = "App Location"
+  description = "App Location"
 }
 
 # MODULE SPECIFIC
 
 variable "api_pass" {
-    description = "api server - root pwd"
-    sensitive = true
+  description = "api server - root pwd"
+  sensitive   = true
 }
 
 variable "api_root_user" {
-    description = "api server root user"
-    sensitive = true
-}   
+  description = "api server root user"
+  sensitive   = true
+}
 
 variable "api_srv_name" {
-    description = "DB Server Name"
+  description = "DB Server Name"
 }
 
 variable "api_srv_size" {
-    default = "Standard_DS1_v2"
+  default = "Standard_DS1_v2"
 }
 
 variable "api_srv_disk_name" {
-    default = "DISK_API_EHEALTH"
+  default = "DISK_API_EHEALTH"
 }
 
 variable "api_srv_disk_storage_account_type" {
-    default = "Premium_LRS"
+  default = "Premium_LRS"
 }
 
 variable "api_srv_distrib" {
-    default = "UbuntuServer"
+  default = "UbuntuServer"
 }
 
 variable "api_srv_distrib_sku" {
-    default = "18.04-LTS"
+  default = "18.04-LTS"
 }
 
 variable "api_srv_distrib_version" {
-    default = "latest"
+  default = "latest"
 }
 
 variable "api_nic_id" {
-    type = list(string)
-    description = "NIC ID"
+  type        = list(string)
+  description = "NIC ID"
 }
 
 variable "dis_pwd_auth" {
-    description = "Disable password authentication (default : false)"
-    default = false
+  description = "Disable password authentication (default : false)"
+  default     = false
 }
 
 variable "api_disk_caching" {
-    description = "Define type of disk access (default : ReadWrite)"
-    default = "ReadWrite"
+  description = "Define type of disk access (default : ReadWrite)"
+  default     = "ReadWrite"
 }
 
-variable api_src_img_ref_publisher {
-    description = "publisher of the image used"
-    default = "Canonical"
+variable "api_src_img_ref_publisher" {
+  description = "publisher of the image used"
+  default     = "Canonical"
 }
 
 variable "api_tags" {
-    type = map(string)
-    default = {
-      "env" = "prod"
-    }
+  type = map(string)
+  default = {
+    "env" = "prod"
+  }
 }

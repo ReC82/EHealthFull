355beb0978c576d68875d004b37c6040d96c08a9
JPSANCHEZ2024 <157719772+JPSANCHEZ2024@users.noreply.github.com>
2024-03-19 13:17:17 +0100

    add all variables

---
+++ terraform/module_security-groups_variables_declarations.tf
@@ -0,0 +1,44 @@
+
+# Variable for resource_group
+variable "nsg_rg_group_name" {
+    type        = string
+    default     = ""
+    description = "RG name in Azure"
+}
+
+variable "nsg_rg_location" {
+    type        = string
+    default     = "" 
+    description = "RG location in Azure"
+}
+
+# Variable for network_security_group
+variable "nsg_app_name" {
+    type        = string
+    default     = ""
+    description = " APP NSG Ehealth in Azure"
+}
+
+variable "nsg_api_java_name" {
+    type        = string
+    default     = ""
+    description = "API NSG Ehealth in Azure"
+}
+
+variable "nsg_db_mysql_name" {
+    type        = string
+    default     = ""
+    description = "DB NSG Ehealth in Azure"
+}
+
+# Variable environement
+variable "nsg_tags" {
+    type = map(string)
+    default = {
+      "env" = "prod"
+    }
+     description = "Environment name for resource group Eahlth"
+}
+
+
+
